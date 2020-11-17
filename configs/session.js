987156dcaa2bbfff8b0385e20da27a4bf7593eaf
Emad Ghnim <emad.ghnim@outlook.com>
2020-11-17 10:45:41 +0100

    fixed session timing

--- configs/session.js
+++ configs/session.js
@@ -18,7 +18,7 @@ module.exports = (incomingApp) => {
       name: "appCookie",
       resave: false,
       saveUninitialized: true,
-      cookie: { maxAge: 60000 }, // 60 * 1000 ms === 1 min
+      cookie: { maxAge: 60000 * 60 * 24 }, // 60 * 1000 ms === 1 min
       store: new MongoStore({
         mongooseConnection: mongoose.connection,
         // ttl => time to live
